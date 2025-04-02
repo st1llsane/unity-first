8d530fd0625dbfb7b5a69f94095355ab2825240c
st1llsane <notescape262@gmail.com>
2025-04-02 22:48:39 +0200

    init

---
+++ Assets/Objects/Player/PlayerMovement.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+  private Rigidbody2D body;
+  private SpriteRenderer sprite;
+  private bool isLastMoveDirectionRight = true;
+  [SerializeField] private float moveSpeed;
+
+  private void Awake()
+  {
+    body = GetComponent<Rigidbody2D>();
+    sprite = GetComponent<SpriteRenderer>();
+    body.mass = 10;
+    body.gravityScale = 6;
+  }
+
+  private void Update()
+  {
+    float horizontalInput = Input.GetAxis("Horizontal") * moveSpeed;
+
+    body.linearVelocity = new Vector2(horizontalInput, body.linearVelocity.y);
+    HandleSpriteFlip();
+
+    if (Input.GetKey(KeyCode.Space))
+    {
+      body.linearVelocity = new Vector2(body.linearVelocity.x, moveSpeed);
+    }
+
+    HandleRestartGame();
+  }
+
+  private void HandleSpriteFlip()
+  {
+    HandleLastMoveDirection();
+    FlipSpriteInRelationOnLastMoveDirection();
+  }
+
+  private void HandleLastMoveDirection()
+  {
+    if (Input.GetKey(KeyCode.A))
+    {
+      isLastMoveDirectionRight = false;
+    }
+    else if (Input.GetKey(KeyCode.D))
+    {
+      isLastMoveDirectionRight = true;
+    }
+  }
+
+  private void FlipSpriteInRelationOnLastMoveDirection()
+  {
+    if (isLastMoveDirectionRight)
+    {
+      FlipSpriteRight();
+    }
+    else
+    {
+      FlipSpriteLeft();
+    }
+  }
+
+  private void FlipSpriteRight()
+  {
+    sprite.flipX = false;
+  }
+
+  private void FlipSpriteLeft()
+  {
+    sprite.flipX = true;
+  }
+
+  private void HandleRestartGame()
+  {
+    if (Input.GetKey(KeyCode.R))
+    {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+  }
+}
