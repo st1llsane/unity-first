c2a43a3e91b73f70122fe5efa8d33e559a3e9d9a
st1llsane <notescape262@gmail.com>
2025-04-03 23:47:12 +0200

    feat(player): add run animation and changing state between Idle and Move

--- Assets/Objects/Player/PlayerMovement.cs
+++ Assets/Objects/Player/PlayerMovement.cs
@@ -1,33 +1,64 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
+  [SerializeField] private float moveSpeed;
   private Rigidbody2D body;
   private SpriteRenderer sprite;
+  private Animator animator;
   private bool isLastMoveDirectionRight = true;
-  [SerializeField] private float moveSpeed;
+  private bool isMoving = false;
+
+  // AWAKE
 
   private void Awake() {
+    RegisterComponents();
+    SetInitialValues();
+  }
+
+  private void RegisterComponents() {
     body = GetComponent<Rigidbody2D>();
     sprite = GetComponent<SpriteRenderer>();
+    animator = GetComponent<Animator>();
+  }
+
+  private void SetInitialValues() {
     body.transform.position = new Vector2(0, -1.85f);
     body.mass = 10;
     body.gravityScale = 6;
   }
 
+  // UPDATE
+
   private void Update() {
-    float horizontalInput = Input.GetAxis("Horizontal") * moveSpeed;
-
-    body.linearVelocity = new Vector2(horizontalInput, body.linearVelocity.y);
-    HandleSpriteFlip();
-
-    if (Input.GetKey(KeyCode.Space)) {
-      body.linearVelocity = new Vector2(body.linearVelocity.x, moveSpeed);
-    }
+    HandleMovement();
+    HandleAnimations();
 
     HandleRestartGame();
     HandleQuitGame();
   }
+
+  // MOVEMENT
+
+  private void HandleMovement() {
+    HandleLeftRightMoves();
+    HandleSpriteFlip();
+    HandleJump();
+  }
+
+  private void HandleLeftRightMoves() {
+    float horizontalInput = Input.GetAxis("Horizontal") * moveSpeed;
+    body.linearVelocity = new Vector2(horizontalInput, body.linearVelocity.y);
+    HandleIsMoving(horizontalInput);
+
+  }
+
+  private void HandleIsMoving(float horizontalInput) {
+    isMoving = horizontalInput != 0;
+  }
+
+  // SPRITE FLIP
 
   private void HandleSpriteFlip() {
     HandleLastMoveDirection();
@@ -59,6 +90,23 @@ public class Player : MonoBehaviour {
   private void FlipSpriteLeft() {
     sprite.flipX = true;
   }
+
+  // JUMP 
+
+  private void HandleJump() {
+    if (Input.GetKey(KeyCode.Space)) {
+      body.linearVelocity = new Vector2(body.linearVelocity.x, moveSpeed);
+    }
+  }
+
+  // ANIMATIONS
+
+  private void HandleAnimations() {
+    animator.SetBool("Move", isMoving);
+  }
+
+
+  // GAME STATE
 
   private void HandleRestartGame() {
     if (Input.GetKey(KeyCode.R)) {
